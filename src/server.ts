454e3d573cebda576ba024a157dd6c5a3ada4068
Gury Traub <gurytraub@Gurys-MacBook-Air.local>
2023-06-22 23:56:55 +0300

    colliding with top and bottom wallsgit status

--- src/server.ts
+++ src/server.ts
@@ -50,17 +50,25 @@ const createGame = (): Game => {
 
   const paddleWidth = 10;
   const paddleHeight = 80;
-  const paddleOpts = { isStatic: true };
+  const paddleOpts = { isStatic: false, isSensor: true };
 
-  const player1 = Matter.Bodies.rectangle(paddleWidth, 200, paddleWidth, paddleHeight, paddleOpts);
-  const player2 = Matter.Bodies.rectangle(800 - paddleWidth, 200, paddleWidth, paddleHeight, paddleOpts);
+  const player1 = Matter.Bodies.rectangle(paddleWidth, 160, paddleWidth, paddleHeight, paddleOpts);
+  player1.label = "player1";
+  const player2 = Matter.Bodies.rectangle(800 - paddleWidth - 10, 160, paddleWidth, paddleHeight, paddleOpts);
+  player2.label = "player2";
 
-  const ball = Matter.Bodies.circle(150, 200, 10);
-  Matter.Body.setPosition(ball, {x: 50, y: 50});
+  const ball = Matter.Bodies.rectangle(30, 30, 6, 6, {isSensor: true});
+  ball.label = "ball";
+  Matter.Body.setVelocity(ball, {x: 2, y: 2});
+  ball.friction = 0;
+  ball.frictionAir = 0;
 
-  const wallTop = Matter.Bodies.rectangle(400, -10, 800, 20, { isStatic: true });
-  const wallBottom = Matter.Bodies.rectangle(400, 410, 800, 20, { isStatic: true });
+  const wallTop = Matter.Bodies.rectangle(0, 0, 800, 5, { isStatic: true });
+  wallTop.label = "wallTop";
 
+  const wallBottom = Matter.Bodies.rectangle(0, 395, 800, 5, { isStatic: true });
+  wallBottom.label = "wallBottom";
+  
   Matter.World.add(world, [player1, player2, ball, wallTop, wallBottom]);
 
   const game: Game = {
@@ -72,6 +80,44 @@ const createGame = (): Game => {
     wallTop,
     wallBottom,
   };
+
+  Matter.Events.on(engine, 'collisionStart', event => {
+    const pairs = event.pairs;
+    console.log('collision');
+    console.log({a: pairs[0].bodyA, b: pairs[0].bodyB});
+    pairs.forEach(pair => {
+      // Check if the ball collides with a paddle
+      if (
+        (pair.bodyA === game.ball && pair.bodyB === game.player1) ||
+        (pair.bodyA === game.player1 && pair.bodyB === game.ball) ||
+        (pair.bodyA === game.ball && pair.bodyB === game.player2) ||
+        (pair.bodyA === game.player2&& pair.bodyB === game.ball)
+      ) {
+        // Reverse the ball's velocity in the x-axis
+        const oldV = game.ball.velocity.x;
+        const newV = -oldV;
+        // game.ball.velocity.x = newV;
+        Matter.Body.setVelocity(ball, {x: newV, y: game.ball.velocity.y})
+        console.log('changed v.x from ' + oldV + ' to ' + newV);
+      }
+
+      if (
+        (pair.bodyA === game.ball && pair.bodyB === game.wallTop) ||
+        (pair.bodyA === game.wallTop && pair.bodyB === game.ball) ||
+        (pair.bodyA === game.ball && pair.bodyB === game.wallBottom) ||
+        (pair.bodyA === game.wallBottom&& pair.bodyB === game.ball)
+      ) {
+          // Reverse the ball's velocity in the x-axis
+
+
+        const oldV = game.ball.velocity.y;
+        const newV = -oldV;
+        //game.ball.velocity.y = newV;
+        Matter.Body.setVelocity(ball, {x: game.ball.velocity.x, y: newV})
+        console.log('changed v.y from ' + oldV + ' to ' + newV);
+      }
+    });
+  });
 
   return game;
 };
@@ -86,9 +132,9 @@ const getGameState = () => {
 };
 
 const updateGame = () => {
-  Matter.Engine.update(game!.engine);
+  Matter.Engine.update(game!.engine);  
   const s = getGameState();
-  // console.log({state: s});
+  // console.log(s.wallBottom.position?.x, s.wallBottom.position?.y, s.ball.position?.x, s.ball.position?.y);
   io.emit('gameState', s);
 };
 
@@ -142,18 +188,17 @@ io.on('connection', socket => {
 
   socket.on('disconnect', () => {
     console.log(`Player disconnected: ${socket.id}`);
-    if (players[socket.id]) {
-      const playerNumber = players[socket.id];
-      delete players[socket.id];
+    // if (players[socket.id]) {
+    //   delete players[socket.id];
       
-      if (game) {
-        Matter.World.remove(game.world, [game.player1, game.player2]);
-      }
+    //   if (game) {
+    //     Matter.World.remove(game.world, [game.player1, game.player2]);
+    //   }
 
-      if (Object.keys(players).length === 0) {
-        resetGame();
-      }
-    }
+    //   if (Object.keys(players).length === 0) {
+    resetGame();
+    //   }
+    // }
   });
 });
 
